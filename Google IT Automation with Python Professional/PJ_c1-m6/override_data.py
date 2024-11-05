58f7c4c74776a3156a2e161e2a2020fb2e67b1f5
Lycoris A <39981286+ask-lycoris@users.noreply.github.com>
2024-11-05 17:30:45 +0900

    Create override_data.py

---
+++ Google IT Automation with Python Professional/PJ_c1-m6/override_data.py
@@ -0,0 +1,8 @@
+content = """events = [
+    Event('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
+    Event('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
+    Event('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
+]"""
+
+with open('events.py', 'w') as file:
+    file.write(content)
