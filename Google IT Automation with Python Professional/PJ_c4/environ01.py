a4f35693b84ee7f157b85c281f9f97eada0b6655
Lycoris A <39981286+ask-lycoris@users.noreply.github.com>
2024-11-07 14:28:06 +0900

    Update environ01.py

--- Google IT Automation with Python Professional/PJ_c4/environ01.py
+++ Google IT Automation with Python Professional/PJ_c4/environ01.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python3
+# スクリプトにコマンドライン引数で特定の値を受け取らせるのはIaCでも非常に役立つ！
+# これによりスクリプトの汎用化ができ、インタラクティブなユーザ入力を必要とせずに自動的に実行することが可能である。
+# システム管理に必須なスキル
 
+#!/usr/bin/env python3
+import sys
+print(sys.argv)
+
+# $ ./parameters.py one two three 
+# ---> パラメータ3つ渡すと、['./parameters.py','one','two','three']のようにリスト型に格納されて出力される！
+# The list of arguments are stored in the sys module.
+# このプログラムにパラメータ(引数)を渡してスクリプトを回すと、そのデータがsys moduleの中に格納されて一緒に出力される。
+
+
+#!/usr/bin/env python3
 import os
 import sys
 
