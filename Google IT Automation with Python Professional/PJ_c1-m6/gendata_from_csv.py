2a59b6d0652870835e7723a603bc546affe814af
Lycoris A <39981286+ask-lycoris@users.noreply.github.com>
2024-11-08 00:35:35 +0900

    Update gendata_from_csv.py

--- Google IT Automation with Python Professional/PJ_c1-m6/gendata_from_csv.py
+++ Google IT Automation with Python Professional/PJ_c1-m6/gendata_from_csv.py
@@ -1,14 +1,14 @@
 import csv
 
 # CSVファイルのパス
-csv_file_path = 'events.csv'
+csv_file = 'events.csv'
 
 # データを格納するリスト
 events = []
 
 try:
     # CSVファイルを開く
-    with open(csv_file_path, 'r', encoding='utf-8') as file:
+    with open(csv_file, 'r', encoding='utf-8') as file:
         reader = csv.reader(file)  # CSVリーダーを作成
         
         # 各行をリストに追加
@@ -16,7 +16,7 @@ try:
             events.append(row)
 
 except FileNotFoundError:
-    print(f"{csv_file_path} が見つかりません。")
+    print(f"{csv_file} が見つかりません。")
 except Exception as e:
     print(f"エラーが発生しました: {e}")
 
