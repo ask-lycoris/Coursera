90a522be2048b8e598585043bd03a927b859a64d
Lycoris A <39981286+ask-lycoris@users.noreply.github.com>
2024-11-10 22:58:07 +0900

    Update m5_unittest02.py

--- Google IT Automation with Python Professional/PJ_c2/m5_unittest02.py
+++ Google IT Automation with Python Professional/PJ_c2/m5_unittest02.py
@@ -18,13 +18,34 @@ from rearrange import rearrange_name
 
 # The name of class should be Test..., prefix. All methods begin with Test will be executed automatically in test framework.
 class TestRearrange(unittest.TestCase):
-    
+# 対象関数の基本的な挙動確認テスト
   def test_basic(self):
     testcase = "Lovelace, Ada"
     expected = "Ada Lovelace"
   # 継承したテストケースクラスから提供されたassertEqualメソッドを使用
     self.assertEqual(rearrange_name(testcase), expected)
 
+# Edge cases: Inputs to ourcode that produce unexpected results, and found at the extreme ends of the ranges of input we image our programs will typically work with.
+# 予期しない結果をもたらすコードへの入力
+　def test_empty(self):
+    testcase = ""
+    expected = ""
+    self.assertEqual(rearrange_name(test_case), expected)   # NameError
+
+# Additional Test Cases: Create other test cases with your imagination.
+def test_double_name(self):
+    testcase = "Hopper, Grace M."
+    expected = "Grace M. Hopper"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+def test_one_name(self):
+    testcase = "Voltaire"
+    expected = "Voltaire"
+    self.assertEqual(rearrange_name(testcase), expected)   # AssertionError
+  
+# イメージは、関数をつくりながら、その関数がどんな結果をもたらすかのテストコードを書いてチェックしながら目的の関数が出来上がるまでバグやエラーハンドリングを追加する等修正し続ける、そんな感じ
+# なお、修正する箇所はなるべく少ない方が良いので、複数のエラーの原因を作っている箇所を特定し問題を集約して最適解を修正として盛り込むのがベスト！
+                     
 # Run the tests
 # Entry point for conducting tests
 unittest.main()
