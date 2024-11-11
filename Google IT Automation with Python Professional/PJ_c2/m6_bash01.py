304abefd43aff9fe4da3087ddac57889df9133e5
Lycoris A <39981286+ask-lycoris@users.noreply.github.com>
2024-11-11 21:45:16 +0900

    Update m6_bash01.py

--- Google IT Automation with Python Professional/PJ_c2/m6_bash01.py
+++ Google IT Automation with Python Professional/PJ_c2/m6_bash01.py
@@ -1,1 +1,34 @@
+# Google colab内で他の言語を使用する際は、「%%(=マジックコマンド)」を先頭につけて '%%javascript' のように指定するとその言語で動かしてくれる。
+%%bash
 
+echo "Starting at: $(date)"
+echo
+
+echo "UPTIME"
+uptime
+echo
+
+echo "FREE"
+free
+echo
+
+echo "WHO"
+whoami
+echo
+
+echo "Finishing at: $(date)"
+
+# output
+# Starting at: Mon Nov 11 12:37:12 PM UTC 2024
+# UPTIME
+#  12:37:12 up 6 min,  0 users,  load average: 0.49, 0.55, 0.31
+
+# FREE
+#                total        used        free      shared  buff/cache   available
+# Mem:        13290460      926416     7257112        1708     5106932    12036924
+# Swap:              0           0           0
+
+# WHO
+# root
+
+# Finishing at: Mon Nov 11 12:37:12 PM UTC 2024
