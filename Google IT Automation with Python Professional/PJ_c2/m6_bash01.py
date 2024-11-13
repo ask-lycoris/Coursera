19766f49682a12f78585e4395f09d1b474ab6066
Lycoris A <39981286+ask-lycoris@users.noreply.github.com>
2024-11-13 16:38:18 +0900

    Update m6_bash01.py

--- Google IT Automation with Python Professional/PJ_c2/m6_bash01.py
+++ Google IT Automation with Python Professional/PJ_c2/m6_bash01.py
@@ -100,3 +100,9 @@ for file in *.HTM; do
   name=$(basename "$file" .HTM)
   mv "$file" "$name.html"
 done
+
+### sample6: Loop
+for logfile in /var/log/*log; do
+    echo "Processing: $logfile"
+    cut -d' ' -f5- $logfile | sort | uniq -c | sort -nr | head -5
+done
