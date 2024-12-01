818f5cb22e0b671f0d8f49328636def3e0dfa4fa
Lycoris A <39981286+ask-lycoris@users.noreply.github.com>
2024-12-01 14:10:45 +0900

    Update git_command02.py

--- Google IT Automation with Python Professional/PJ_c3/git_command02.py
+++ Google IT Automation with Python Professional/PJ_c3/git_command02.py
@@ -52,3 +52,33 @@ git log --oneline
 
 git log --oneline
 は各コミットを一行で表示します。
+
+git remote 
+
+$ git remote
+ allows you to manage the set of repositories or “remotes” whose branches you track.
+
+git remote -v
+
+$ git remote -v
+ is similar to $ git remote, but adding the -v shows more information such as the remote URL.
+
+git remote show <name>
+
+$ git remote show <name>
+ shows some information about a single remote repo.
+
+git remote update
+
+$ git remote update
+ fetches updates for remotes or remote groups.
+
+git fetch
+
+$ git fetch
+ can download objects and refs from a single repo, a single URL, or from several repositories at once.
+
+git branch -r
+
+$ git branch -r
+ lists remote branches and can be combined with other branch arguments to manage remote branches.
