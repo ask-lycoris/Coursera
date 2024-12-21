c0c4e96d454b94c79e2c4c0aa7a474be787dc17c
Lycoris A <39981286+ask-lycoris@users.noreply.github.com>
2024-12-21 11:49:20 +0900

    Update GCP.py

--- Google IT Automation with Python Professional/PJ_c4/GCP.py
+++ Google IT Automation with Python Professional/PJ_c4/GCP.py
@@ -1,2 +1,5 @@
 The gcloud init command sets up the authentication procedure between our virtual machine and Google Cloud.
 Ensures each node receives a reasonable number of requests.
+
+Automation is when we set up a single step in a process to require no oversight, while orchestration refers to automating the entire process.
+Automation refers to a single task; orchestration arranges tasks to optimize a workflow
